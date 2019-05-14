97466a138932ff187677b7bc7d178e26f5c3b56d
chinmayapalo <chinmayarajapalo@gmail.com>
2019-05-14 13:21:37 +0530

    adding the code to github

---
+++ src/main/java/com/chinmaya/test/Maven_Junit_test/App.java
@@ -0,0 +1,13 @@
+package com.chinmaya.test.Maven_Junit_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
